22ede7fb8dbb59c6dfb8380a43954dfab9ff2acc
Grant Randa <48031522+grantranda@users.noreply.github.com>
2020-10-28 11:58:01 -0400

    Change width/height to int from double

--- src/main/java/com/grantranda/retorn/app/state/DisplayState.java
+++ src/main/java/com/grantranda/retorn/app/state/DisplayState.java
@@ -2,8 +2,8 @@ package com.grantranda.retorn.app.state;
 
 public class DisplayState {
 
-    private double width;
-    private double height;
+    private int width;
+    private int height;
     private boolean vSync;
 
     public DisplayState() {
