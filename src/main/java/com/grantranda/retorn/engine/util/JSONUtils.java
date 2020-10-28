df81203f17f4326882f85cd51195e40d5cc92bac
Grant Randa <48031522+grantranda@users.noreply.github.com>
2020-10-28 14:42:14 -0400

    Add JSON utility class

---
+++ src/main/java/com/grantranda/retorn/engine/util/JSONUtils.java
@@ -0,0 +1,42 @@
+package com.grantranda.retorn.engine.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JSONUtils {
+
+    private JSONUtils() {
+
+    }
+
+    public static <T> T readJson(Class<T> type, String path) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader(path));
+        T object = new Gson().fromJson(in, type);
+        in.close();
+        return object;
+    }
+
+    public static String toJson(Object object) {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .serializeNulls()
+                .create();
+        return gson.toJson(object);
+    }
+
+    public static void toJson(Object object, String path) throws IOException {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .serializeNulls()
+                .create();
+        BufferedWriter out = new BufferedWriter(new FileWriter(path));
+        gson.toJson(object, out);
+        out.close();
+    }
+}
