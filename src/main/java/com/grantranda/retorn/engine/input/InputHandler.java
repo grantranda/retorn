d8d6a47dd19b9911dae9e12953ecf494d8e5af70
grantranda <48031522+grantranda@users.noreply.github.com>
2020-10-31 18:07:14 -0400

    Remove shader from handle()

--- src/main/java/com/grantranda/retorn/engine/input/InputHandler.java
+++ src/main/java/com/grantranda/retorn/engine/input/InputHandler.java
@@ -1,10 +1,9 @@
 package com.grantranda.retorn.engine.input;
 
-import com.grantranda.retorn.engine.graphics.Shader;
 import com.grantranda.retorn.engine.graphics.Window;
 import com.grantranda.retorn.engine.state.State;
 
 public interface InputHandler {
 
-    void handle(Window window, Shader shader, State state);
+    void handle(Window window, State state);
 }
