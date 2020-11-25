566f00cb1eebba0cb3a4fd53aa8f8137aa997de5
grantranda <48031522+grantranda@users.noreply.github.com>
2020-11-25 14:30:37 -0500

    Make final fields uppercase

--- src/main/java/com/grantranda/retorn/engine/graphics/Mesh.java
+++ src/main/java/com/grantranda/retorn/engine/graphics/Mesh.java
@@ -12,31 +12,31 @@ public class Mesh {
     public static int VBO_ATTRIB_INDEX = 0;
     public static int TBO_ATTRIB_INDEX = 1;
 
-    private final int vaoId, vboId, tboId, iboId;
-    private final int vertexCount;
+    private final int VAO_ID, VBO_ID, TBO_ID, IBO_ID;
+    private final int VERTEX_COUNT;
 
     public Mesh(float[] vertices, float[] textureCoordinates, byte[] indices) {
-        vertexCount = indices.length;
+        VERTEX_COUNT = indices.length;
 
-        vaoId = glGenVertexArrays();
-        glBindVertexArray(vaoId);
+        VAO_ID = glGenVertexArrays();
+        glBindVertexArray(VAO_ID);
 
-        vboId = glGenBuffers();
-        glBindBuffer(GL_ARRAY_BUFFER, vboId);
+        VBO_ID = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_ID);
         glBufferData(GL_ARRAY_BUFFER, MemoryUtils.allocateFloatBuffer(vertices), GL_STATIC_DRAW);
         glVertexAttribPointer(VBO_ATTRIB_INDEX, 3, GL_FLOAT, false, 0, 0);
         glEnableVertexAttribArray(VBO_ATTRIB_INDEX);
 
-        tboId = glGenBuffers();
+        TBO_ID = glGenBuffers();
         if (textureCoordinates != null) {
-            glBindBuffer(GL_ARRAY_BUFFER, tboId);
+            glBindBuffer(GL_ARRAY_BUFFER, TBO_ID);
             glBufferData(GL_ARRAY_BUFFER, MemoryUtils.allocateFloatBuffer(textureCoordinates), GL_STATIC_DRAW);
             glVertexAttribPointer(TBO_ATTRIB_INDEX, 2, GL_FLOAT, false, 0, 0);
             glEnableVertexAttribArray(TBO_ATTRIB_INDEX);
         }
         
-        iboId = glGenBuffers();
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboId);
+        IBO_ID = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO_ID);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, MemoryUtils.allocateByteBuffer(indices), GL_STATIC_DRAW);
 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // Unbind IBO
@@ -45,24 +45,24 @@ public class Mesh {
     }
 
     public void bind() {
-        glBindVertexArray(vaoId);
-        if (iboId > 0) {
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboId);
+        glBindVertexArray(VAO_ID);
+        if (IBO_ID > 0) {
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO_ID);
         }
     }
 
     public void unbind() {
-        if (iboId > 0) {
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboId);
+        if (IBO_ID > 0) {
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO_ID);
         }
         glBindVertexArray(0);
     }
 
     public void draw() {
-        if (iboId > 0) {
-            glDrawElements(GL_TRIANGLES, vertexCount, GL_UNSIGNED_BYTE, 0);
+        if (IBO_ID > 0) {
+            glDrawElements(GL_TRIANGLES, VERTEX_COUNT, GL_UNSIGNED_BYTE, 0);
         } else {
-            glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+            glDrawArrays(GL_TRIANGLES, 0, VERTEX_COUNT);
         }
     }
 
