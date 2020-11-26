0f7936878a274f161148bff98016e513c5cdbd42
grantranda <48031522+grantranda@users.noreply.github.com>
2020-11-26 12:29:35 -0500

    Add Framebuffer class for custom framebuffers

---
+++ src/main/java/com/grantranda/retorn/engine/graphics/Framebuffer.java
@@ -0,0 +1,55 @@
+package com.grantranda.retorn.engine.graphics;
+
+import java.util.ArrayList;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL30.*;
+
+public class Framebuffer {
+
+    private final int FBO;
+
+    private final ArrayList<Texture> textures = new ArrayList<>();
+
+    public Framebuffer() {
+
+        // Create framebuffer
+        FBO = glGenFramebuffers();
+        glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+    }
+
+    public int getFBO() {
+        return FBO;
+    }
+
+    public ArrayList<Texture> getTextures() {
+        return textures;
+    }
+
+    public void bind() {
+        glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+    }
+
+    public void unbind() {
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    }
+
+    public void delete() {
+        glDeleteFramebuffers(FBO);
+    }
+
+    public boolean isComplete() {
+        return glCheckFramebufferStatus(FBO) == GL_FRAMEBUFFER_COMPLETE;
+    }
+
+    public int addColorAttachment(int attachment, int width, int height) {
+        bind();
+
+        Texture texture = new Texture(GL_TEXTURE_2D, GL_RGB, GL_LINEAR, width, height);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_2D, texture.getID(), 0);
+        textures.add(texture);
+
+        unbind();
+        return texture.getID();
+    }
+}
