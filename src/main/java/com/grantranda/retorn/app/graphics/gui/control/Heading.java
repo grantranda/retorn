f18f0181be9194747976cbf8e04efa3a524bea8e
Grant Randa <48031522+grantranda@users.noreply.github.com>
2021-05-01 08:20:32 -0400

    Create Heading

---
+++ src/main/java/com/grantranda/retorn/app/graphics/gui/control/Heading.java
@@ -0,0 +1,18 @@
+package com.grantranda.retorn.app.graphics.gui.control;
+
+import lwjgui.font.FontStyle;
+import lwjgui.geometry.Insets;
+import lwjgui.geometry.Pos;
+import lwjgui.scene.control.Label;
+
+public class Heading extends Label {
+
+    public Heading(String text) {
+        super(text);
+
+        setFillToParentWidth(true);
+        setAlignment(Pos.CENTER_LEFT);
+        setFontStyle(FontStyle.BOLD);
+        setPadding(new Insets(0, 0, 0, 0));
+    }
+}
