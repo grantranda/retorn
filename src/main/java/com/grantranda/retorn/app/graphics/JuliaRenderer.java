4f38823525fc4e96a45584ac636087a5bb241298
Grant Randa <48031522+grantranda@users.noreply.github.com>
2021-05-23 17:16:30 -0400

    Add default seed constants

--- src/main/java/com/grantranda/retorn/app/graphics/JuliaRenderer.java
+++ src/main/java/com/grantranda/retorn/app/graphics/JuliaRenderer.java
@@ -1,14 +1,33 @@
 package com.grantranda.retorn.app.graphics;
 
+import com.grantranda.retorn.engine.math.Vector2d;
+
 public class JuliaRenderer extends AbstractFractalRenderer {
+
+    public static final double DEFAULT_SEED_X = -0.8;
+    public static final double DEFAULT_SEED_Y = 0.156;
+
+    protected final Vector2d seed = new Vector2d(DEFAULT_SEED_X, DEFAULT_SEED_Y);
 
     public JuliaRenderer(String vertexShaderPath, String fragmentShaderPath) {
         super(vertexShaderPath, fragmentShaderPath);
     }
 
+    public Vector2d getSeed() {
+        return seed;
+    }
+
+    public void setSeedX(double x) {
+        seed.x = x;
+    }
+
+    public void setSeedY(double y) {
+        seed.y = y;
+    }
+
     @Override
     protected void setUniforms() {
-        super.shader.setUniform2d("seed", -0.8, 0.156);
+        super.shader.setUniform2d("seed", seed.x, seed.y);
         super.shader.setUniform2d("trappingPointOffset", trappingPointOffset.x, trappingPointOffset.y);
     }
 }
