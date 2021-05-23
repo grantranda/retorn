8621c57874ccc9ff9d590aea4cf915bfe2b61204
Grant Randa <48031522+grantranda@users.noreply.github.com>
2021-05-23 09:29:03 -0400

    Add coloringAlgorithm & escapeRadius fields to RenderState

--- src/main/java/com/grantranda/retorn/app/state/RenderState.java
+++ src/main/java/com/grantranda/retorn/app/state/RenderState.java
@@ -1,6 +1,7 @@
 package com.grantranda.retorn.app.state;
 
 import com.grantranda.retorn.app.Retorn;
+import com.grantranda.retorn.app.graphics.ColoringAlgorithm;
 import com.grantranda.retorn.engine.graphics.display.Resolution;
 import com.grantranda.retorn.engine.math.Vector3d;
 import com.grantranda.retorn.engine.state.State;
@@ -8,21 +9,25 @@ import com.grantranda.retorn.engine.state.State;
 public class RenderState implements State {
 
     public static final String DEFAULT_FRACTAL_ALGORITHM = Retorn.MANDELBROT_SET;
+    public static final ColoringAlgorithm DEFAULT_COLORING_ALGORITHM = ColoringAlgorithm.ESCAPE_TIME;
     public static final int DEFAULT_WIDTH = 1280;
     public static final int DEFAULT_HEIGHT = 720;
     public static final int DEFAULT_MAX_ITERATIONS = 100;
+    public static final int DEFAULT_ESCAPE_RADIUS = ColoringAlgorithm.ESCAPE_TIME.getEscapeRadius();
     public static final double DEFAULT_OFFSET = 0.0;
     public static final double DEFAULT_SCALE = 1.0;
 
     private final Resolution renderResolution = new Resolution(DEFAULT_WIDTH, DEFAULT_HEIGHT);
     private final Vector3d offset = new Vector3d(DEFAULT_OFFSET, DEFAULT_OFFSET, DEFAULT_OFFSET);
     private String fractalAlgorithm = DEFAULT_FRACTAL_ALGORITHM;
+    private ColoringAlgorithm coloringAlgorithm = DEFAULT_COLORING_ALGORITHM;
     private int maxIterations = DEFAULT_MAX_ITERATIONS;
+    private int escapeRadius = DEFAULT_ESCAPE_RADIUS;
     private double scale = DEFAULT_SCALE;
     private boolean customResolution = false;
     private boolean fractalAspectRatioMaintained = true;
 
-    // Palette class?
+    // TODO: Palette class?
 
     public RenderState() {
 
@@ -36,12 +41,28 @@ public class RenderState implements State {
         this.fractalAlgorithm = fractalAlgorithm;
     }
 
+    public ColoringAlgorithm getColoringAlgorithm() {
+        return coloringAlgorithm;
+    }
+
+    public void setColoringAlgorithm(ColoringAlgorithm coloringAlgorithm) {
+        this.coloringAlgorithm = coloringAlgorithm;
+    }
+
     public int getMaxIterations() {
         return maxIterations;
     }
 
     public void setMaxIterations(int maxIterations) {
         this.maxIterations = maxIterations;
+    }
+
+    public int getEscapeRadius() {
+        return escapeRadius;
+    }
+
+    public void setEscapeRadius(int escapeRadius) {
+        this.escapeRadius = escapeRadius;
     }
 
     public double getScale() {
@@ -89,7 +110,9 @@ public class RenderState implements State {
     @Override
     public void reset() {
         setFractalAlgorithm(DEFAULT_FRACTAL_ALGORITHM);
+        setColoringAlgorithm(DEFAULT_COLORING_ALGORITHM);
         setMaxIterations(DEFAULT_MAX_ITERATIONS);
+        setEscapeRadius(DEFAULT_ESCAPE_RADIUS);
         setScale(DEFAULT_SCALE);
         setOffset(DEFAULT_OFFSET, DEFAULT_OFFSET, DEFAULT_OFFSET);
         setRenderResolution(DEFAULT_WIDTH, DEFAULT_HEIGHT);
